73e8f1d7a5528c9031108709e9a153f145a8973c
Antony Charles <anthonyraj0399@gmail.com>
2025-06-04 19:18:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def result():
         print(type(inputs))
 
         if " CMM CNC                                                              \nPART NAME      " in inputs:
-            reading=reading[970:]
+            reading=reading[978:]
             reads="p name"
         else:
             
